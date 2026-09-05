6bc227683bd4fad959aff47637ab57383491a917
NikhilllGowda21 <nikhilllgowda217@gmail.com>
2026-09-05 22:24:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("1 "))
+b = int(input("6 "))
 
 print("Addition:", a + b)
 print("Subtraction:", a - b)
